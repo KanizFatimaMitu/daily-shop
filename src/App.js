a08893c5682acc9c72432e656639651df64581ab
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-06-29 12:52:41 +0600

    react component library added perfectly

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>start new web</h1>
+      <button className="btn btn-active">Default</button>
+<button className="btn btn-active btn-neutral">Neutral</button>
+<button className="btn btn-active btn-primary">Primary</button>
+<button className="btn btn-active btn-secondary">Secondary</button>
+<button className="btn btn-active btn-accent">Accent</button>
+<button className="btn btn-active btn-ghost">Ghost</button>
+<button className="btn btn-active btn-link">Link</button>
     </div>
   );
 }
