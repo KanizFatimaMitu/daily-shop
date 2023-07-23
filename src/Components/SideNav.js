de64c0fd020a2112555699b5c03fa4cab2a718eb
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-07-23 12:41:09 +0600

    content divided by css perfectly

---
+++ src/Components/SideNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SideNav = () => {
+    return (
+        <div>
+            <h1>this will be nav</h1>
+        </div>
+    );
+};
+
+export default SideNav;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Home.css'
+import SideNav from '../Components/SideNav'
+import Content from '../Components/Content'
+import AddToCart from '../Components/AddToCart'
 
 const Home = () => {
     return (
         <div className='appGlass'>
-            <h1>
-            this is home
-
-            </h1>
+           <SideNav></SideNav>
+           <Content></Content>
+           <AddToCart></AddToCart>
         </div>
     );
 };
