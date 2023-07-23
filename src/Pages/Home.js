93bb1765b8ecb7398d5f31beea7cc9a840374f0b
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-07-23 12:34:43 +0600

    glass effect added

---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className='appGlass'>
+            <h1>
+            this is home
+
+            </h1>
+        </div>
+    );
+};
+
+export default Home;
