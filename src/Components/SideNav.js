098ed420f45b5da4f550a9252e0c6976554eebbb
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-07-25 23:17:44 +0600

    will do tomorrow

--- src/Components/SideNav.js
+++ src/Components/SideNav.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const SideNav = () => {
     return (
-        <div className='p-4 '>
+        <div className='p-4 w-auto'>
+            <div className='text-center'>
+                {/* <img src=''/> */}
+                <h1>Daily</h1>
+                <br></br>
+                <h1>Shopping</h1>
+            </div>
             <ul className="menu bg-base-200 w-56 rounded-xl ">
                 <li className='text-lg font-bold'><a>All Product</a></li>
                 <li className='text-lg font-bold'><a>Catagory</a></li>
